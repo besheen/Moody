c52b86e100b4388fcd8f2dbc0a648c6578c36e99
Wendell <bisheng.zhong@gmail.com>
2022-08-11 00:01:44 +0800

    create project

---
+++ Moody/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Moody
+//
+//  Created by Wendell on 2022/8/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Moody/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Moody
+//
+//  Created by Wendell on 2022/8/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
